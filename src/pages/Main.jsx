f4ac24bf5fccae7d82d68af6d180f3a6c296ec94
Danee12 <9356471@gmail.com>
2023-05-29 16:53:16 +0900

    Main 페이지 공항 select 박스 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,31 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import * as style from "../styles/style";
+import { useSelector } from "react-redux";
+import AirportSelect from "../components/common/AirportSelect";
 
 function Main() {
-  const mainPhrase = '즐겨 찾는 여행 사이트를 빠르고 쉽게 검색하세요'
+  const mainPhrase = "즐겨 찾는 여행 사이트를 빠르고 쉽게 검색하세요";
+
+  // 공항을 담고 있는 배열 -> 나중에 db get 으로 변경해야 함(수정 필요)
+  const airportArray = ["인천", "부산", "김해", "제주", "대구", "여수"];
+
+  // 공항 select State
+  const [airport, setAirport] = useState({ departure: "공항선택", arrival: "공항선택" });
+  const { departure, arrival } = airport;
+
+  // onChange 적용 함수
+  const onChangeAirport = (changeObj) => {
+    const { name, value } = changeObj.target;
+
+    const newAirport = {
+      ...airport,
+      [name]: value,
+    };
+
+    setAirport(newAirport);
+  };
+
+  // 버튼 클릭 시 적용 함수
+  const onClickHandler = () => {
+    alert(`출발 공항:${airport.departure} 도착 공항:${airport.arrival}`);
+  };
+
   return (
     <style.MainHeaderBack>
       <style.MainPhrase>{mainPhrase}</style.MainPhrase>
       <style.MainConditionBox>
-        <style.MainSelect>
-          <option key="apple" value="apple">
-            사과
-          </option>
-          <option key="orange" value="orange">
-            오렌지
-          </option>
-        </style.MainSelect>
-        <style.MainSelect>
-          <option key="apple" value="apple">
-            사과
-          </option>
-          <option key="orange" value="orange">
-            오렌지
-          </option>
-        </style.MainSelect>
+        <AirportSelect name="departure" value={departure} onChange={onChangeAirport}/>
+        <AirportSelect name="arrival" value={arrival} onChange={onChangeAirport}/>
         <style.MainCalendar>떠나는 날짜</style.MainCalendar>
         <style.MainPersonNumber>인원 수</style.MainPersonNumber>
-        <style.MainSearchBtn>검색하기</style.MainSearchBtn>
+        <style.MainSearchBtn onClick={onClickHandler}>검색하기</style.MainSearchBtn>
       </style.MainConditionBox>
     </style.MainHeaderBack>
   );
